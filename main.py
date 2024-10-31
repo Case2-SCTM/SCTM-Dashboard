ac914563d4edd1ebe08323f575cfd60d401d7a2f
PandaOnCaffeine <Tuepvm@gmail.com>
2024-10-31 12:13:04 +0100

    plotly file

--- main.py
+++ main.py
@@ -42,3 +42,34 @@ query.awaitTermination()
 
 # if __name__ == "__main__":
 #     main()
+
+
+# from dash import Dash, html, dcc, callback, Output, Input
+# import plotly.express as px
+# import pandas as pd
+# import dash_bootstrap_components as dbc
+# from dash_bootstrap_templates import load_figure_template
+
+# # loads the "darkly" template and sets it as the default
+# load_figure_template("darkly")
+
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+# app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+# app.layout = [
+#     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+#     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+#     dcc.Graph(id='graph-content')
+# ]
+
+# @callback(
+#     Output('graph-content', 'figure'),
+#     Input('dropdown-selection', 'value')
+# )
+# def update_graph(value):
+#     dff = df[df.country==value]
+#     return px.line(dff, x='year', y='pop')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
