f46616849123b1bda5070b2168d6167a0320d9ba
PandaOnCaffeine <Tuepvm@gmail.com>
2024-11-05 14:31:43 +0100

    updated something

--- main.py
+++ main.py
@@ -23,39 +23,59 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 data_queue = Queue()
 streaming_data = []
 
+custom_colors = {
+    "car": "#1f77b4",
+    "bicycle": "#ff7f0e",
+    "van": "#d62728",
+    "pedestrian": "#9467bd",
+    "motorcycle": "#8c564b",
+    "bus": "#e377c2",
+    "heavy": "#2ca02c",
+    "light": "#7f7f7f",
+}
+
 
 # Kafka consumer function
 def kafka_consumer(topic):
     consumer = KafkaConsumer(
         topic,
         bootstrap_servers=["localhost:9092"],
-        auto_offset_reset="latest",
         value_deserializer=lambda x: loads(x.decode("utf-8")),
         consumer_timeout_ms=1000,
     )
     while True:
         for message in consumer:
             data = message.value
-            # Add timestamp if not present
-            if "timestamp" not in data:
-                data["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             data_queue.put(data)
         # Sleep briefly to prevent tight loop if no messages
         sleep(1)
 
 
 @app.callback(
-    [Output("traffic-count-graph", "figure"), Output("traffic-pie-graph", "figure")],
-    [Input("interval-component", "n_intervals")],
-    [State("traffic-count-graph", "figure")],
+    [
+        Output("traffic-count-graph", "figure"),
+        Output("traffic-pie-graph", "figure"),
+        Output("traffic-timeline-graph", "figure"),
+    ],
+    [Input("traffic-interval", "n_intervals")],
+    [
+        State("traffic-count-graph", "figure"),
+        State("traffic-pie-graph", "figure"),
+        State("traffic-timeline-graph", "figure"),
+    ],
 )
-def update_count_graph(n_intervals, existing_figure):
+def updateDistributionGraphs(n, state_bar, state_pie, state_timeline):
     while not data_queue.empty():
         data = data_queue.get()
         streaming_data.append(data)
 
     if not streaming_data:
-        return px.line(title="Waiting for Data...")
+        waiting = (
+            px.bar(title="Waiting for Data..."),
+            px.pie(title="Waiting for Data..."),
+            px.line(title="Waiting for Data..."),
+        )
+        return waiting
 
     # Copy data to avoid threading issues
     data_snapshot = streaming_data.copy()
@@ -63,28 +83,29 @@ def update_count_graph(n_intervals, existing_figure):
     # Convert to DataFrame
     df = pd.DataFrame(data_snapshot)
 
+    # Bar and Pie
     df["count"] = pd.to_numeric(df["count"], errors="coerce")
     df["category"] = df["category"].astype(str)
 
-    # Drop rows with NaN values
-    df.dropna(subset=["count", "category"], inplace=True)
+    # Timeline
+    df["start_timestamp"] = pd.to_datetime(
+        df["start_timestamp"], unit="s", errors="coerce"
+    )
+    df["end_timestamp"] = pd.to_datetime(df["end_timestamp"], unit="s", errors="coerce")
 
-    df_grouped = df.groupby("category", as_index=False)["count"].sum()
+    # Groupped data for bar and pie graph
+    df_grouped_category = df.groupby("category", as_index=False)["count"].sum()
 
-    custom_colors = {
-        "car": "#1f77b4",
-        "bicycle": "#ff7f0e",
-        "van": "#d62728",
-        "pedestrian": "#9467bd",
-        "motorcycle": "#8c564b",
-        "bus": "#e377c2",
-        "heavy": "#2ca02c",
-        "light": "#7f7f7f",
-    }
+    # Groupped data for timeline graph
+    df_grouped_timeline = df.groupby(["start_timestamp", "category"], as_index=False)[
+        "count"
+    ].sum()
 
-    # Create the Plotly figure
-    bar_fig = px.bar(
-        df_grouped,
+    df_grouped_timeline.sort_values("start_timestamp", inplace=True)
+
+    # Create Plotly Bar Graph Figure
+    figure_bar = px.bar(
+        df_grouped_category,
         x="category",
         y="count",
         title="Total Traffic Counts by Vehicle Type",
@@ -93,14 +114,9 @@ def update_count_graph(n_intervals, existing_figure):
         color_discrete_map=custom_colors,
     )
 
-    bar_fig.update_layout(
-        xaxis_title="Vehicle Type",
-        yaxis_title="Total Count",
-        legend_title="Category",
-    )
-
-    pie_fig = px.pie(
-        df_grouped,
+    # Create Plotly Pie Graph Figure
+    figure_pie = px.pie(
+        df_grouped_category,
         names="category",
         values="count",
         title="Traffic Distribution by Vehicle Type",
@@ -108,60 +124,9 @@ def update_count_graph(n_intervals, existing_figure):
         color_discrete_map=custom_colors,
     )
 
-    # Preserve existing layout (like zoom) if an existing figure is provided
-    if existing_figure:
-        bar_fig.update_layout(existing_figure["layout"], overwrite=False)
-
-    return bar_fig, pie_fig
-
-
-@app.callback(
-    Output("traffic-timeline-graph", "figure"),
-    [Input("interval-component", "n_intervals")],
-    # [State("traffic-count-graph", "figure")],
-)
-def update_timeline_graph(n_intervals):
-    while not data_queue.empty():
-        data = data_queue.get()
-        streaming_data.append(data)
-
-    if not streaming_data:
-        return px.line(title="Waiting for data...")
-
-    data_snapshot = streaming_data.copy()
-
-    # Convert data to a DataFrame
-    df = pd.DataFrame(data_snapshot)
-
-    df["count"] = pd.to_numeric(df["count"], errors="coerce")
-    df["category"] = df["category"].astype(str)
-    df["start_timestamp"] = pd.to_datetime(
-        df["start_timestamp"], unit="s", errors="coerce"
-    )
-    df["end_timestamp"] = pd.to_datetime(df["end_timestamp"], unit="s", errors="coerce")
-
-    # Group by timestamp and category to get the total count for each vehicle type per timestamp
-    df_grouped = df.groupby(["start_timestamp", "category"], as_index=False)[
-        "count"
-    ].sum()
-
-    # Sort grouped dataframe by timestamp
-    df_grouped.sort_values("start_timestamp", inplace=True)
-
-    # Define custom colors for each vehicle type
-    custom_colors = {
-        "car": "#1f77b4",
-        "bicycle": "#ff7f0e",
-        "van": "#d62728",
-        "pedestrian": "#9467bd",
-        "motorcycle": "#8c564b",
-        "bus": "#e377c2",
-        "heavy": "#2ca02c",
-        "light": "#7f7f7f",
-    }
-
-    fig = px.line(
-        df_grouped,
+    # Create Plotly Line Graph
+    figure_timeline = px.line(
+        df_grouped_timeline,
         x="start_timestamp",
         y="count",
         title="Total Traffic Counts by Vehicle Type",
@@ -170,18 +135,45 @@ def update_timeline_graph(n_intervals):
         color_discrete_map=custom_colors,
     )
 
-    fig.update_layout(
+    # Update Layout for Bar Graph
+    figure_bar.update_layout(
+        xaxis_title="Vehicle Type",
+        yaxis_title="Total Count",
+        legend_title="Category",
+    )
+
+    # Update Layout for Line Graph
+    figure_timeline.update_layout(
         xaxis_title="Timestamp",
         yaxis_title="Traffic Count",
         legend_title="Vehicle type",
         xaxis=dict(tickformat="%H:%M"),
     )
 
-    # # Preserve existing layout (like zoom) if an existing figure is provided
-    # if existing_figure:
-    #     fig.update_layout(existing_figure["layout"], overwrite=False)
+    # Update layout only if the existing figures are not the "Waiting for Data" placeholders
+    if (
+        state_bar
+        and state_bar.get("layout", {}).get("title", {}).get("text")
+        != "Waiting for Data..."
+    ):
+        figure_bar.update_layout(state_bar["layout"], overwrite=False)
 
-    return fig
+    if (
+        state_pie
+        and state_pie.get("layout", {}).get("title", {}).get("text")
+        != "Waiting for Data..."
+    ):
+        figure_pie.update_layout(state_pie["layout"], overwrite=False)
+
+    if (
+        state_timeline
+        and state_timeline.get("layout", {}).get("title", {}).get("text")
+        != "Waiting for Data..."
+    ):
+        figure_timeline.update_layout(state_timeline["layout"], overwrite=False)
+
+    # Return Bar, Pie and Line Graph
+    return (figure_bar, figure_pie, figure_timeline)
 
 
 # Define Dash layout with a graph and interval for updates
@@ -192,23 +184,53 @@ app.layout = html.Div(
             brand_href="#",
             color="dark",
             dark=True,
+            children=[
+                dbc.NavItem(
+                    html.Div(
+                        "Lavet af Simon og Tue",
+                        style={
+                            "color": "white",
+                            "fontSize": "16px",
+                            "marginRight": "15px",
+                        },
+                    )
+                )
+            ],
+            expand="lg",
+            className="ml-auto",
         ),
         dbc.Container(
             [
                 dbc.Row(
                     [
-                        dbc.Col(dcc.Graph(id="traffic-count-graph"),style={"padding": "0"}, width=7),
-                        dbc.Col(dcc.Graph(id="traffic-pie-graph"),style={"padding": "0"} , width=5),
+                        dbc.Col(
+                            dcc.Graph(id="traffic-count-graph"),
+                            style={"padding": "1px"},
+                            width=7,
+                        ),
+                        dbc.Col(
+                            dcc.Graph(id="traffic-pie-graph"),
+                            style={"padding": "1px"},
+                            width=5,
+                        ),
                     ]
                 ),
                 dbc.Row(
                     [
-                        dbc.Col(dcc.Graph(id="traffic-timeline-graph"),style={"padding": "0"} , width=8),
-                        dbc.Col(dcc.Graph(id="traffic-statistics-graph"),style={"padding": "0"}, width=4),
+                        dbc.Col(
+                            dcc.Graph(id="traffic-timeline-graph"),
+                            style={"padding": "1px"},
+                            width=8,
+                        ),
+                        dbc.Col(
+                            dcc.Graph(id="traffic-statistics-graph"),
+                            style={"padding": "1px"},
+                            width=4,
+                        ),
                     ]
                 ),
                 dcc.Interval(
-                    id="interval-component", interval=2 * 1000, n_intervals=0
+                    id="traffic-interval", interval=1 * 1000, n_intervals=0
                 ),  # Update every 2 seconds
             ],
             fluid=True,
--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ from threading import Thread
 
 
 def runThread(func):
-    thread = Thread(target=func, daemon=True)
+    thread = Thread(name=f"Func_{func}", target=func, daemon=True)
     thread.start()
 
 
 def runThread1Arg(func, arg1):
-    thread = Thread(target=func, args=(arg1,), daemon=True)
+    thread = Thread(name=f"Func_{func}_{arg1}", target=func, args=(arg1,), daemon=True)
     thread.start()
